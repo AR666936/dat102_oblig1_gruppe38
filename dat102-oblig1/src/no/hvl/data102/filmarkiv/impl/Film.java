3548cf62ae7f0caccd24a0ce2cdf1245f765a58e
AR666936 <alek123@live.no>
2024-01-25 18:04:12 +0100

    hei

---
+++ dat102-oblig1/src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -0,0 +1,101 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import java.util.Objects;
+
+public class Film {
+
+	private int filmNr;
+	private String produsent;
+	private String tittel;
+	private int lansering;
+	private Sjanger sjanger;
+	private String filmselskap;
+	
+	public Film() {
+		this.filmNr = 0;
+		this.produsent = null;
+		this.tittel = null;
+		this.lansering = 0;
+		this.sjanger = null;
+		this.filmselskap = null;
+	}
+	
+	public Film(int filmNr, String produsent, String tittel, int lansering, Sjanger sjanger, String filmselskap) {
+		this.filmNr = filmNr;
+		this.produsent = produsent;
+		this.tittel = tittel;
+		this.lansering = lansering;
+		this.sjanger = sjanger;
+		this.filmselskap = filmselskap;
+	}
+
+	public int getFilmNr() {
+		return filmNr;
+	}
+
+	public void setFilmNr(int filmNr) {
+		this.filmNr = filmNr;
+	}
+
+	public String getProdusent() {
+		return produsent;
+	}
+
+	public void setProdusent(String produsent) {
+		this.produsent = produsent;
+	}
+
+	public String getTittel() {
+		return tittel;
+	}
+
+	public void setTittel(String tittel) {
+		this.tittel = tittel;
+	}
+
+	public int getLansering() {
+		return lansering;
+	}
+
+	public void setLansering(int lansering) {
+		this.lansering = lansering;
+	}
+
+	public Sjanger getSjanger() {
+		return sjanger;
+	}
+
+	public void setSjanger(Sjanger sjanger) {
+		this.sjanger = sjanger;
+	}
+
+	public String getFilmselskap() {
+		return filmselskap;
+	}
+
+	public void setFilmselskap(String filmselskap) {
+		this.filmselskap = filmselskap;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(filmNr);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		return filmNr == other.filmNr;
+	}
+
+	
+
+	
+	
+}
---
+++ dat102-oblig1/src/no/hvl/data102/filmarkiv/klient/Tekstgrensesnitt.java
@@ -0,0 +1,90 @@
+package no.hvl.data102.filmarkiv.klient;
+
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+
+public class Tekstgrensesnitt {
+
+	// Leser inn opplysninger om en film fra tastatur og returnere et Film-objekt
+	public Film lesFilm(){
+	
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		System.out.println("Film nr: ");
+		int filmNr = parseInt(input.nextLine());
+		if (finnFilm()getfilmNr())
+		System.out.println("Produsent: ");
+		String produsent = input.nextLine();
+		System.out.println("Tittel: ");
+		String tittel = input.nextLine();
+		System.out.println("År: ");
+		int lansering = parseInt(input.nextLine());
+		System.out.println("Sjanger: ");
+		Sjanger sjanger = Sjanger.finnSjanger(input.nextLine());
+		System.out.println("Filmselskap: ");
+		String filmselskap = input.nextLine();
+		Film film = new Film(filmNr, produsent, tittel, lansering, sjanger, filmselskap);
+		
+		return film;
+		
+	}
+	// Skriver ut en film med alle opplysninger på skjerm (husk tekst for sjanger)
+	public void skrivUtFilm(Film film) {
+		
+		if(film == null) {
+			System.out.println("Finner ingen filmer med det nr.");
+		} else {
+		
+		System.out.println("Film nr: " + film.getFilmNr());
+		System.out.println("Produsent: " + film.getProdusent());
+		System.out.println("Tittel: " + film.getTittel());
+		System.out.println("År: " + film.getLansering());
+		System.out.println("Sjanger: " + film.getSjanger());
+		System.out.println("Filmselskap: " + film.getFilmselskap());
+		System.out.println();
+		}
+		
+	}
+	// Skriver ut alle filmer med en spesiell delstreng i tittelen
+	public void skrivUtFilmDelstrengITittel(FilmarkivADT arkiv, String delstreng) {
+	
+		Film[] resultat = arkiv.soekTittel(delstreng);
+		System.out.println("Resultat: ");
+		for(int i = 0; i < resultat.length; i++) {
+			skrivUtFilm(resultat[i]);
+			System.out.println();
+		}
+		System.out.println();
+		
+	}
+	// Skriver ut alle Filmer av en produsent (produsent er delstreng)
+	public void skrivUtFilmProdusent(FilmarkivADT arkiv, String delstreng) {
+	
+		Film[] resultat = arkiv.soekProdusent(delstreng);
+		System.out.println("Resultat: ");
+		for(int i = 0; i < resultat.length; i++) {
+			skrivUtFilm(resultat[i]);
+			System.out.println();
+		}
+		System.out.println();
+	}
+	// Skriver ut en enkel statistikk som inneholder antall filmer totalt
+	// og hvor mange det er i hver sjanger.
+	public void skrivUtStatistikk(FilmarkivADT arkiv) {
+	
+		System.out.println("Antall filmer: " + arkiv.antall());
+		System.out.println("Antall actionfilmer: " + arkiv.antall(Sjanger.ACTION));
+		System.out.println("Antall dramafilmer: " + arkiv.antall(Sjanger.DRAMA));
+		System.out.println("Antall historiefilmer: " + arkiv.antall(Sjanger.HISTORY));
+		System.out.println("Antall scififilmer: " + arkiv.antall(Sjanger.SCIFI));
+		System.out.println("Antall komediefilmer: " + arkiv.antall(Sjanger.COMEDY));
+		System.out.println();
+		
+	}
+	// osv ... andre metoder
+	
+}
